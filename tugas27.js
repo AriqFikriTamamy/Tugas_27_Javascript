9d8f6e96a3705ac121c29d2c1d08f631a104eab9
Ariq Fikri Tamamy <ariqrick@gmail.com>
2021-08-04 21:37:26 +0700

    Tugas 27 Javascript

---
+++ tugas27.js
@@ -0,0 +1,19 @@
+function konversi(a,b,c,d){
+  var a = "No";
+  var b = " rumah saya ";
+  var c = 30;
+  var d = " A"
+
+  c = String(c);
+  var result1 = a + b + c + d;
+  console.log(result1)
+
+  var e = "Jalan Pangeran Tirtayasa No "
+  var f = 70
+
+  f = String(f);
+  var result2 = e + f;
+  console.log(result2)
+}
+
+konversi()
